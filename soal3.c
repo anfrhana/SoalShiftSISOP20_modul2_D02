cb847b4f35a4445411188ff56e24a075907b6a01
anfrhana <61223768+anfrhana@users.noreply.github.com>
2020-03-13 17:08:47 +0700

    Update soal3.c

--- soal3.c
+++ soal3.c
@@ -40,7 +40,7 @@ int main() {
 	execv("/bin/mkdir", argv);
 	}
 ///////////////////////////////////////////////////////////////////////////////////
-	else
+	else{
 
 	while((wait(&status)) > 0);
 	
@@ -55,76 +55,8 @@ int main() {
      	execv("/usr/bin/unzip", unzip);
 	}
 //////////////////////////////////////////////////////////////////////////////////
-	else 
-    {
 
-	while ((wait(&status)) > 0);
-
-	chdir("/home/hana/modul2/jpg/");
-	DIR *d;
-	d = opendir(".");
-	struct dirent *dir;
-
-	if(d){
-
-        while((dir = readdir(d)) != NULL){
-
-	pid4 = fork();
-
-	struct stat st;
-	stat(dir->d_name, &st);
-
-	if(pid4 < 0){
-	exit(EXIT_FAILURE);
-	}
-
-	if(pid4== 0){
-	char file[500];
-	sprintf(file, "/home/hana/modul2/jpg/%s", dir->d_name);
-
-	if(S_ISDIR(st.st_mode)){
-		if(strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0);
-	else
-      {
-
-	pid5 = fork();
-
-	if(pid5 < 0){
-	exit(EXIT_FAILURE);
-	}
-	
-	if(pid5 == 0){
-	char* argv[] = {"mv", file, "/home/hana/modul2/indomie/", NULL};
-	execv("/bin/mv", argv);
-	}
-///////////////////////////////////////////////////////////////////////////////////
-	else{
-
-	while((dir = readdir(d)) != NULL){
-
-	pid6 = fork();
-
-	if(pid6 == 0){
-	chdir("/home/hana/modul2/jpg/");
-	char *mk[]={"touch","/home/hana/modul2/jpg/coba1.txt",NULL};
-        execv("/bin/touch",mk);
-	}
-
-	sleep(3);
-
-	pid7 = fork();
-
-	if(pid7 == 0){
-	chdir("/home/hana/modul2/jpg/");
-	char *mk[]={"touch","/home/hana/modul2/jpg/coba2.txt",NULL};
-        execv("/bin/touch",mk);
 	}
 	}
-	}
-	closedir(d);
-}}}
-
 }
-
-
 
